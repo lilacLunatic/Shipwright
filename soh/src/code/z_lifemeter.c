e01b367c0f9bad4b946ae2fbc320a2cc8fe7f6be
Andrew Van Caem <andrewwvc@gmail.com>
2022-06-04 10:54:33 +1000

    Revert "Added consistant life meter line length mod"
    This reverts commit 849e168d654d61f3f9266eeba0d5504579144a1b.

--- soh/src/code/z_lifemeter.c
+++ soh/src/code/z_lifemeter.c
@@ -571,7 +571,7 @@ void HealthMeter_Draw(GlobalContext* globalCtx) {
         }
 
         offsetX += 10.0f;
-        if ((i+1)%10 == 0) {
+        if (i == 9) {
             offsetY += 10.0f;
             offsetX = OTRGetDimensionFromLeftEdge(0.0f)+(Left_LM_Margin*-1);
         }
