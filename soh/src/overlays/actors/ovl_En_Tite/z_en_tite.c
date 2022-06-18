884dd1c2d5c917a64c1a35a404d92d01108d7d42
Andrew Van Caem <andrewwvc@gmail.com>
2022-06-19 05:51:29 +1000

    Refactored detection code

--- soh/src/overlays/actors/ovl_En_Tite/z_en_tite.c
+++ soh/src/overlays/actors/ovl_En_Tite/z_en_tite.c
@@ -518,25 +518,24 @@ void EnTite_SetupMoveTowardPlayer(EnTite* this, GlobalContext* globalCtx) {
     this->vQueuedJumps = Rand_S16Offset(0, 3);
     
     f32 distance = 60.0*(this->vQueuedJumps+1);
-    s16 angle = (s16)(this->actor.shape.rot.y + 0x4000);
-    f32 dx = Math_SinS(angle) * distance;
-    f32 dz = Math_CosS(angle) * distance;
-    Vec3f newPos = this->actor.world.pos;
-    newPos.x += dx;
-    newPos.z += dz;
-    Vec3f finalPos;
-    s32 bgId;
-    s32 wallHitL = BgCheck_EntitySphVsWall3(&globalCtx->colCtx,&finalPos,&newPos, &this->actor.prevPos,20.0f,
-                &this->actor.wallPoly, &bgId, &this->actor, 5.0f);
-    newPos = this->actor.world.pos;
-    newPos.x -= dx;
-    newPos.z -= dz;
-    s32 wallHitR = BgCheck_EntitySphVsWall3(&globalCtx->colCtx,&finalPos,&newPos, &this->actor.prevPos,20.0f,
-                &this->actor.wallPoly, &bgId, &this->actor, 5.0f);
-    //s32 wallHitR = EnTite_ProjectWallColision(this, globalCtx, distance, (s16)0x4000+(s16)0x4000+(s16)0x4000);
+    s16 angle = 0x4000;
+    //f32 dx = Math_SinS(angle) * distance;
+    //f32 dz = Math_CosS(angle) * distance;
+    //Vec3f newPos = this->actor.world.pos;
+    //newPos.x += dx;
+    //newPos.z += dz;
+    //Vec3f finalPos;
+    //s32 bgId;
+    s32 wallHitL = EnTite_ProjectWallColision(this, globalCtx, distance, angle);
+    //newPos = this->actor.world.pos;
+    //newPos.x -= dx;
+    //newPos.z -= dz;
+    //s32 wallHitR = BgCheck_EntitySphVsWall3(&globalCtx->colCtx,&finalPos,&newPos, &this->actor.prevPos,20.0f,
+                //&this->actor.wallPoly, &bgId, &this->actor, 5.0f);
+    s32 wallHitR = EnTite_ProjectWallColision(this, globalCtx, distance, -angle);//(s16)0x4000+(s16)0x4000+(s16)0x4000
     
-    this->actor.world.rot.y = this->actor.yawTowardsPlayer +
-                (wallHitL ? (wallHitR ? 0 : angle) : (wallHitR ? -angle : (Rand_ZeroOne() < 0.5f ? angle : -angle)) );
+    this->actor.world.rot.y = this->actor.yawTowardsPlayer + 
+                (wallHitL ? (wallHitR ? 0 : -angle) : (wallHitR ? angle : (Rand_ZeroOne() < 0.5f ? -angle : angle)) );
     this->actor.velocity.y = MOVEMENT_SPEED;
     this->actor.gravity = -1.0f;
     this->actor.speedXZ = MOVEMENT_SPEED;
