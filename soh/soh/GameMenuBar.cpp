f0e30b5678efe38d1704015da1f38d68332c8a5e
RaelCappra <rael.cappra@gmail.com>
2022-12-09 16:14:33 -0300

    if SILLY guards for the menu

--- soh/soh/GameMenuBar.cpp
+++ soh/soh/GameMenuBar.cpp
@@ -382,23 +382,25 @@ namespace GameMenuBar {
                     UIWidgets::PaddedText("Bunny Hood Effect", true, false);
                     const char* bunnyHoodOptions[3] = { "Disabled", "Faster Run & Longer Jump", "Faster Run"};
                     UIWidgets::EnhancementCombobox("gMMBunnyHood", bunnyHoodOptions, 3, 0);
-                    if (ImGui::IsItemHovered()) {
-                        char* textBunny = \
-                        "Wearing the Bunny Hood grants a speed increase like in Majora's Mask. The longer jump option is not accounted for in randomizer logic.\n\n"
-                        "Also disables NPC's reactions to wearing the Bunny Hood.\n";
-                        ImGui::BeginTooltip();
-                        ImGui::Text("%s", UIWidgets::WrappedText(textBunny, 60));
-                        ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 50, 50, 255));
-                        ImGui::Text("%s", UIWidgets::WrappedText("this has been cursed", 60));
-                        ImGui::PopStyleColor();
-                        ImGui::EndTooltip();
+                    if (SILLY) {
+                        if (ImGui::IsItemHovered()) {
+                            char* textBunny = \
+                            "Wearing the Bunny Hood grants a speed increase like in Majora's Mask. The longer jump option is not accounted for in randomizer logic.\n\n"
+                            "Also disables NPC's reactions to wearing the Bunny Hood.\n";
+                            ImGui::BeginTooltip();
+                            ImGui::Text("%s", UIWidgets::WrappedText(textBunny, 60));
+                            ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 50, 50, 255));
+                            ImGui::Text("%s", UIWidgets::WrappedText("this has been cursed", 60));
+                            ImGui::PopStyleColor();
+                            ImGui::EndTooltip();
+                        }
                     }
-                    /*
-                    UIWidgets::Tooltip(
-                        "Wearing the Bunny Hood grants a speed increase like in Majora's Mask. The longer jump option is not accounted for in randomizer logic.\n\n"
-                        "Also disables NPC's reactions to wearing the Bunny Hood."
-                    );
-                    */
+                    else {
+                        UIWidgets::Tooltip(
+                            "Wearing the Bunny Hood grants a speed increase like in Majora's Mask. The longer jump option is not accounted for in randomizer logic.\n\n"
+                            "Also disables NPC's reactions to wearing the Bunny Hood."
+                        );
+                    }
                     UIWidgets::PaddedEnhancementCheckbox("Mask Select in Inventory", "gMaskSelect", true, false);
                     UIWidgets::Tooltip("After completing the mask trading sub-quest, press A and any direction on the mask slot to change masks");
                     UIWidgets::PaddedEnhancementCheckbox("Nuts explode bombs", "gNutsExplodeBombs", true, false);
@@ -406,19 +408,23 @@ namespace GameMenuBar {
                     UIWidgets::PaddedEnhancementCheckbox("Equip Multiple Arrows at Once", "gSeparateArrows", true, false);
                     UIWidgets::Tooltip("Allow the bow and magic arrows to be equipped at the same time on different slots");
                     UIWidgets::PaddedEnhancementCheckbox("Turn Nayru's Love into Roc's Feather", "gRocsFeather", true, false);
-                    if (ImGui::IsItemHovered()) {
-                        char* text = "Nayru's Love acts like Roc's Feather from the Indigo romhack instead. Grants a jump that can even be used in the air.";
-                        ImGui::BeginTooltip();
-                        ImGui::Text("%s", UIWidgets::WrappedText(text, 60));
-                        ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 50, 50, 255));
-                        ImGui::Text("%s", UIWidgets::WrappedText("But at what cost?", 60));
-                        ImGui::PopStyleColor();
-                        ImGui::EndTooltip();
+                    if (SILLY) {
+                        if (ImGui::IsItemHovered()) {
+                            char* text = "Nayru's Love acts like Roc's Feather from the Indigo romhack instead. Grants a jump that can even be used in the air.";
+                            ImGui::BeginTooltip();
+                            ImGui::Text("%s", UIWidgets::WrappedText(text, 60));
+                            ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 50, 50, 255));
+                            ImGui::Text("%s", UIWidgets::WrappedText("But at what cost?", 60));
+                            ImGui::PopStyleColor();
+                            ImGui::EndTooltip();
+                        }
                     }
-                    /*UIWidgets::Tooltip(
-                        "Nayru's Love acts like Roc's Feather from the Indigo romhack instead. Grants a jump that can even be used in the air, "
-                        "but needs to recharge by touching the ground afterwards. Does not require magic to use."
-                    );*/
+                    else {
+                        UIWidgets::Tooltip(
+                            "Nayru's Love acts like Roc's Feather from the Indigo romhack instead. Grants a jump that can even be used in the air, "
+                            "but needs to recharge by touching the ground afterwards. Does not require magic to use."
+                        );
+                    }
                     ImGui::EndMenu();
                 }
 
