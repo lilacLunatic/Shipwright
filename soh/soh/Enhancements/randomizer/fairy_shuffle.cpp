e655f2e77e49c64756ece79c2948ff92fcb4a3a4
Angel Bulfone <mbulfone@gmail.com>
2024-09-04 00:34:59 -0700

    Add field to fairy entities to hold randomizer data

--- soh/soh/Enhancements/randomizer/fairy_shuffle.cpp
+++ soh/soh/Enhancements/randomizer/fairy_shuffle.cpp
@@ -1,5 +1,6 @@
 #include "soh/Enhancements/game-interactor/GameInteractor_Hooks.h"
 #include "fairy_shuffle.h"
+#include "src/overlays/actors/ovl_En_Elf/z_en_elf.h"
 
 void FairyOnVanillaBehaviorHandler(GIVanillaBehavior id, bool* should, void* optionalArg) {
 	if (id == VB_SPAWN_FAIRY_GROUP) {
@@ -7,14 +8,23 @@ void FairyOnVanillaBehaviorHandler(GIVanillaBehavior id, bool* should, void* opt
 	}
 }
 
+void FairyOnActorInitHandler(void* actorRef) {
+	EnElf* enElf = static_cast<EnElf*>(actorRef);
+	enElf->sohFairyIdentity = { RAND_INF_MAX, GET_ITEM_NONE };
+}
+
 uint32_t onVanillaBehaviorHook = 0;
+uint32_t onActorInitHook = 0;
 
 void FairyRegisterHooks() {
 	onVanillaBehaviorHook = GameInteractor::Instance->RegisterGameHook<GameInteractor::OnVanillaBehavior>(FairyOnVanillaBehaviorHandler);
+	onActorInitHook = GameInteractor::Instance->RegisterGameHookForID<GameInteractor::OnActorInit>(ACTOR_EN_ELF, FairyOnActorInitHandler);
 }
 
 void FairyUnregisterHooks() {
     GameInteractor::Instance->UnregisterGameHook<GameInteractor::OnVanillaBehavior>(onVanillaBehaviorHook);
+    GameInteractor::Instance->UnregisterGameHookForID<GameInteractor::OnActorInit>(onActorInitHook);
 
     onVanillaBehaviorHook = 0;
+    onActorInitHook = 0;
 }
