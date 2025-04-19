3042e15c95c56350aaeb50bd9d2d9008c3b8f9b7
lilacLunatic <8488221+lilacLunatic@users.noreply.github.com>
2025-04-18 21:27:07 -0300

    please build winmac 2

--- soh/soh/OTRGlobals.cpp
+++ soh/soh/OTRGlobals.cpp
@@ -616,7 +616,7 @@ extern "C" void OTRAudio_Init()
 
             auto stream = std::make_shared<ZAPD::MemoryStream>();
             ZAPD::BinaryWriter writerObj = ZAPD::BinaryWriter(stream);
-            ZAPD::BinaryWriter writer = &writerObj;
+            ZAPD::BinaryWriter* writer = &writerObj;
             writer->Write((uint8_t)0); // 0x00
             writer->Write((uint8_t)0); // 0x01
             writer->Write((uint8_t)0); // 0x02
